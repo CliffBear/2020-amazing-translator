6a01ddc7eedca295e923971c599bb94761775854
CliffBear <jackhulls@hotmail.com>
2020-08-17 15:21:08 +1200

    REQ_5

--- src/main/java/nz/ac/eit/amazingtranslator/App.java
+++ src/main/java/nz/ac/eit/amazingtranslator/App.java
@@ -18,34 +18,58 @@ public class App
         frenchTranslations.add("Deux");
         frenchTranslations.add("Trois");
 
-        System.out.println( "What number shall we translate?" );
-        Scanner scanner=new Scanner(System.in);
-        String numberAsString=scanner.nextLine();
-        Integer number = null;
-        try {
-            number = Integer.parseInt(numberAsString);
-            System.out.println(number);
-        }
-        catch (NumberFormatException nfe) {
-            System.out.println("The Number must be entered in Numeric form");
-            System.exit(0);
-        }
-        System.out.println( "What language shall we translate into?" );
-        System.out.println("1:French");
-        System.out.println("2:German");
-        String optionAsString=scanner.nextLine();
-        Integer option= null;
-        try {
-            option = Integer.parseInt(optionAsString);
-        }
-        catch (NumberFormatException nfe) {
-            System.out.println("The Option must be entered in Numeric form");
-            System.exit(0);
-        }
+        Boolean recursive= true;
+        while(recursive = true) {
+            System.out.println( "What number shall we translate?" );
+            Scanner scanner=new Scanner(System.in);
+            String numberAsString=scanner.nextLine();
+            Integer number = null;
+            try {
+                number = Integer.parseInt(numberAsString);
+                System.out.println(number);
+            }
+            catch (NumberFormatException nfe) {
+                System.out.println("The Number must be entered in Numeric form");
+                System.exit(0);
+            }
+            System.out.println( "What language shall we translate into?" );
+            System.out.println("1:French");
+            System.out.println("2:German");
+            String optionAsString=scanner.nextLine();
+            Integer option= null;
+            try {
+                option = Integer.parseInt(optionAsString);
+            }
+            catch (NumberFormatException nfe) {
+                System.out.println("The Option must be entered in Numeric form");
+                System.exit(0);
+            }
 
-        if (option ==1){
-            String translated=frenchTranslations.get(number-1);
-            System.out.println(translated);
+            if (option ==1){
+                String translated=frenchTranslations.get(number-1);
+                System.out.println(translated);
+            } else{
+                String translated=germanTranslations.get(number-1);
+                System.out.println(translated);
+            }
+            System.out.println("Would you like to Translate another number?");
+            System.out.println("1:Yes");
+            System.out.println("2:No");
+            String loopOptionString=scanner.nextLine();
+            Integer loop= null;
+            try {
+                loop = Integer.parseInt(loopOptionString);
+            }
+            catch (NumberFormatException nfe) {
+                System.out.println("The Option must be entered in Numeric form");
+                System.exit(0);
+            }
+
+            if(loop ==1){
+                recursive = true;
+            } else {
+                System.exit(0);
+            }
         }
     }
 }
